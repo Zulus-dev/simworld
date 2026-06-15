53a156b3edfaafad524bf001d3ad0ee3e427a4e2
Zulus-dev <antareszulus@gmail.com>
2026-06-15 11:46:02 +0300

    Delete core directory

--- core/soa_views.py
+++
@@ -1,8 +0,0 @@
-import torch
-from typing import Dict
-
-from .memory import MemoryManager
-
-def create_soa_views(memory: MemoryManager) -> Dict[str, torch.Tensor]:
-    """Centralized safe SoA view creation"""
-    return memory.get_soa_views()
